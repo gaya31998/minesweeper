b1a6e384139a4f74b2464405eb7761f55c58e5af
Gayathri Alagusubramanian <gayathri.alagusubramanian@zoomrx.com>
2019-08-05 15:35:11 +0530

    Updates

---
+++ precache-manifest.7da6769a33e6fee0b5b418146aecfef9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961ec54cfa36ff4157d3b49c28913968",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "3190cf895689392abe55",
+    "url": "/minesweeper/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "fc14dde44c3f7a9e698d",
+    "url": "/minesweeper/static/css/main.7799574b.chunk.css"
+  },
+  {
+    "revision": "3190cf895689392abe55",
+    "url": "/minesweeper/static/js/2.124178fe.chunk.js"
+  },
+  {
+    "revision": "fc14dde44c3f7a9e698d",
+    "url": "/minesweeper/static/js/main.3c296c03.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  },
+  {
+    "revision": "47b921366bb4b60a824b7cfda80f19e2",
+    "url": "/minesweeper/static/media/bomb.47b92136.svg"
+  }
+]);
